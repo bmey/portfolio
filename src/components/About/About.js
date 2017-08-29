ad645c32f26c92fa61efc79f6706962584c9344a
Bryan Mey <bryan.j.mey@gmail.com>
2017-08-28 21:23:50 -0400

    Add contact/work teaser section to about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -118,13 +118,29 @@ class About extends Component {
           </div>
 
           <div className="section">
-            <h2>Want to talk?</h2>
-            <p>
-              Lorem ipsum dolor sit amet. Sed et posuere mauris. Nam vitae ipsum vel id euismod magna. Etia congue pharetra felis.
-            </p>
-            <Link to="/contact">
-              <Button bsStyle="primary">Contact me</Button>
-            </Link>
+            <Grid>
+              <Row>
+                <Col xs={12} md={6}>
+                  <h2>Ready to start a project?</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet. Sed et posuere mauris. Etia congue pharetra felis.
+                  </p>
+                  <Link to="/contact">
+                    <Button bsStyle="primary">Contact me</Button>
+                  </Link>
+                </Col>
+                <Col xs={12} md={6}>
+                  <b>Still not convinced?</b>
+                  <p>
+                    Lorem ipsum dolor sit amet. Etia congue pharetra felis. Sed et posuere mauris.
+                  </p>
+                  <Link to="/work">
+                    Learn more about what I do
+                  </Link>
+                </Col>
+              </Row>
+            
+            </Grid>
           </div>
         </div>
     );
