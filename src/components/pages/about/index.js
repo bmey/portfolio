bdf4410cb8b7f16a23d9142ecdae690802bf42ea
Bryan Mey <bryan.j.mey@gmail.com>
2017-11-22 08:40:58 -0500

    Use hero section on about page. Style quote text in hero section in CSS Grid.

--- src/components/pages/about/index.js
+++ src/components/pages/about/index.js
@@ -1,35 +1,46 @@
 import React, { Component } from 'react';
-import Hero from './Hero';
 import Skills from './Skills';
 import Experiences from './Experiences';
 import Story from './Story';
 import CallToAction from './CallToAction';
 import Section from '../../section/Section';
+import HeroSection from '../../section-hero';
+import heroImage from '../../../img/splash-1250x500.jpg';
+import './section-hero.css';
 
 class About extends Component {
   render() {
     return (
-      <div className="text-center">
-        <Hero />
+      <div>
+        <h1>About</h1>
+        <div className="text-center">
+          <HeroSection className="about section" backgroundImage={heroImage}>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+              bibendum lorem nec maximus venenatis. Sed et posuere mauris. Nunc
+              sed facilisis lectus. Nunc sit amet dui scelerisque."
+            </p>
+          </HeroSection>
 
-        <Section>
-          <h2>Skills</h2>
-          <Skills />
-        </Section>
+          <Section>
+            <h2>Skills</h2>
+            <Skills />
+          </Section>
 
-        <Section useSecondaryBackground>
-          <h2>Experience</h2>
-          <Experiences />
-        </Section>
+          <Section useSecondaryBackground>
+            <h2>Experience</h2>
+            <Experiences />
+          </Section>
 
-        <Section>
-          <h2>My Story</h2>
-          <Story />
-        </Section>
+          <Section>
+            <h2>My Story</h2>
+            <Story />
+          </Section>
 
-        <Section useSecondaryBackground>
-          <CallToAction />
-        </Section>
+          <Section useSecondaryBackground>
+            <CallToAction />
+          </Section>
+        </div>
       </div>
     );
   }
