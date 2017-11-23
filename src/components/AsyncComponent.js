135ad22fc68e516dfa6900cb2240034622e2c76c
Bryan Mey <bryan.j.mey@gmail.com>
2017-11-23 15:49:48 -0500

    refactor AsyncComponent to use es7 property initializers

--- src/components/AsyncComponent.js
+++ src/components/AsyncComponent.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 
 export default function asyncComponent(importComponent) {
   class AsyncComponent extends Component {
-    constructor(props) {
-      super(props);
-
-      this.state = {
-        component: null
-      };
-    }
+    state = {
+      component: null
+    };
 
     async componentDidMount() {
       const { default: component } = await importComponent();
@@ -19,9 +15,9 @@ export default function asyncComponent(importComponent) {
     }
 
     render() {
-      const C = this.state.component;
+      const ImportedComponent = this.state.component;
 
-      return C ? <C {...this.props} /> : null;
+      return ImportedComponent ? <ImportedComponent {...this.props} /> : null;
     }
   }
 
