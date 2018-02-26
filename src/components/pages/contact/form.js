16da7fb171191a58c6458ab7a66d575c898b6ba1
Bryan Mey <bryan.j.mey@gmail.com>
2018-02-25 22:31:07 -0500

    WIP add beginnings of submitting email to emailJS server with recaptcha from google.

--- src/components/pages/contact/form.js
+++ src/components/pages/contact/form.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import sendEmail from "./submitForm";
 
 export default class Form extends Component {
   state = {
     name: "",
     email: "",
-    description: "",
+    message: "",
   };
 
   handleInputChange(event) {
@@ -17,9 +18,34 @@ export default class Form extends Component {
     });
   }
 
+  isFormValid() {
+    const { name, email, message } = this.state;
+    return (
+      name.length > 0 &&
+      !name.trim() &&
+      email.length > 0 &&
+      !email.trim() &&
+      message.length > 0 &&
+      !message.trim()
+    );
+  }
+
+  handleSubmit(event) {
+    if (this.isFormValid()) {
+      event.preventDefault();
+      sendEmail(this.state);
+      console.log("valid form. sending mail...");
+    } else {
+      console.log("invalid form");
+    }
+  }
+
   render() {
     return (
-      <form className="d-flex flex-column text-left">
+      <form
+        onSubmit={e => this.handleSubmit(e)}
+        className="d-flex flex-column text-left"
+      >
         <p className="d-flex flex-column">
           <label htmlFor="name">
             Full name: <abbr title="required">*</abbr>
@@ -47,12 +73,12 @@ export default class Form extends Component {
           />
         </p>
         <p className="d-flex flex-column">
-          <label htmlFor="description">
+          <label htmlFor="message">
             What would you like to discuss? <abbr title="required">*</abbr>
           </label>
           <textarea
-            id="description"
-            name="description"
+            id="message"
+            name="message"
             type="text"
             value={this.state.firstName}
             onChange={e => this.handleInputChange(e)}
@@ -60,9 +86,19 @@ export default class Form extends Component {
             aria-required="true"
           />
           <small className="text-muted">
-            {500 - this.state.description.length} characters left
+            {500 - this.state.message.length} characters left
           </small>
         </p>
+        <div
+          className="g-recaptcha"
+          data-sitekey="6LdDfEgUAAAAAKlhMWyiPZkXPgFVPDsSd5i7Gn61"
+        />
+        <input
+          type="submit"
+          disabled={this.state.isFormValid}
+          value="Send message"
+          className="btn btn-primary"
+        />
       </form>
     );
   }
---
+++ src/components/pages/contact/submitForm.js
@@ -0,0 +1,17 @@
+const sendEmail = function(form) {
+  console.log(form);
+  //   return emailjs.send("gmail", "portfolio", form).then(
+  //     function(response) {
+  //       console.log(
+  //         "SUCCESS. status=%d, text=%s",
+  //         response.status,
+  //         response.text
+  //       );
+  //     },
+  //     function(err) {
+  //       console.log("FAILED. error=", err);
+  //     }
+  //   );
+};
+
+export default sendEmail;
