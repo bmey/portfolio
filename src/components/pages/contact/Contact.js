13b3183f39637ab89a2196a427c6c0e3fdc72096
Bryan Mey <bryan.j.mey@gmail.com>
2018-04-03 18:11:15 -0400

    convert contact page to functional component

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
 import FormContainer from "../../form/FormContainer";
 import Headshot from "../../headshot/Headshot";
@@ -9,78 +9,76 @@ import { headshotSize } from "../../../constant/sizes";
 import ContactSuccess from "./contactSuccess/ContactSuccess";
 import "./contact.css";
 
-class Contact extends Component {
-  render() {
-    const headshotRadius = headshotSize / 2;
-    const { match, history } = this.props;
+const Contact = props => {
+  const headshotRadius = headshotSize / 2;
+  const { match, history } = props;
 
-    return (
-      <div className="text-center">
-        <h1>Contact</h1>
+  return (
+    <div className="text-center">
+      <h1>Contact</h1>
 
-        <div
-          className="d-flex flex-column flex-md-row"
-          style={{ margin: `${headshotRadius}rem 2rem` }}
-        >
-          <div className="contact-section form-section">
-            <Headshot
-              size={headshotSize}
-              style={{ marginTop: `-${headshotRadius}rem` }}
-            />
-            <Route path={`${match.url}/thankyou`} component={ContactSuccess} />
-            <Route
-              exact
-              path={match.url}
-              render={() => (
-                <div>
-                  <p style={{ margin: "2rem" }}>
-                    Lorem ipsum wouebnvowin evownbrv wermv.
-                  </p>
-                  <div style={{ margin: "1rem 2rem" }}>
-                    <FormContainer
-                      onSubmit={() => history.push(`${match.url}/thankyou`)}
-                    />
-                  </div>
+      <div
+        className="d-flex flex-column flex-md-row"
+        style={{ margin: `${headshotRadius}rem 2rem` }}
+      >
+        <div className="contact-section form-section">
+          <Headshot
+            size={headshotSize}
+            style={{ marginTop: `-${headshotRadius}rem` }}
+          />
+          <Route path={`${match.url}/thankyou`} component={ContactSuccess} />
+          <Route
+            exact
+            path={match.url}
+            render={() => (
+              <div>
+                <p style={{ margin: "2rem" }}>
+                  Lorem ipsum wouebnvowin evownbrv wermv.
+                </p>
+                <div style={{ margin: "1rem 2rem" }}>
+                  <FormContainer
+                    onSubmit={() => history.push(`${match.url}/thankyou`)}
+                  />
                 </div>
-              )}
-            />
+              </div>
+            )}
+          />
+        </div>
+
+        <div className="contact-section marketing-section d-flex flex-column justify-content-around align-content-center">
+          <div>Lorem ipsum wouebnvowin evownbrv wermv.</div>
+
+          <div>
+            <div>Prefer to send an email?</div>
+            <a
+              href="mailto:bryan.j.mey+portfolio@gmail.com"
+              className="d-flex align-items-center justify-content-center"
+            >
+              <MdEmail
+                size={20}
+                aria-hidden
+                style={{ marginRight: "0.25rem" }}
+              />
+              <span>bryan.j.mey@gmail.com</span>
+            </a>
           </div>
 
-          <div className="contact-section marketing-section d-flex flex-column justify-content-around align-content-center">
-            <div>Lorem ipsum wouebnvowin evownbrv wermv.</div>
-
-            <div>
-              <div>Prefer to send an email?</div>
-              <a
-                href="mailto:bryan.j.mey+portfolio@gmail.com"
-                className="d-flex align-items-center justify-content-center"
-              >
-                <MdEmail
-                  size={20}
-                  aria-hidden
-                  style={{ marginRight: "0.25rem" }}
+          <div>
+            <div>Follow me on social media</div>
+            <div className="d-flex justify-content-center justify-items-center">
+              {socialMediaLinks.map(handle => (
+                <SocialMediaLink
+                  key={`${handle.title}-contact-page`}
+                  size={40}
+                  {...handle}
                 />
-                <span>bryan.j.mey@gmail.com</span>
-              </a>
-            </div>
-
-            <div>
-              <div>Follow me on social media</div>
-              <div className="d-flex justify-content-center justify-items-center">
-                {socialMediaLinks.map(handle => (
-                  <SocialMediaLink
-                    key={`${handle.title}-contact-page`}
-                    size={40}
-                    {...handle}
-                  />
-                ))}
-              </div>
+              ))}
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Contact;
